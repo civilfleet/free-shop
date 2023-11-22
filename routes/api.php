ae58d5bc5ba2f8b559ef7677b91baf772d8bd865
Qiong Wu <qiong.wu@gfnork.de>
2023-11-22 02:23:27 +0100

    fix

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\StatisticsController;
 
-Route::get('statistics', 'API\StatisticsController@get');
+Route::get('statistics', [StatisticsController::class, 'get']);
