ca323d9d9783c4f3570228a41224252fdf7f1cac
Qiong Wu <qiong.wu@gfnork.de>
2023-11-22 02:21:10 +0100

    add use

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\StatisticsController;
 
 Route::get('statistics', 'API\StatisticsController@get');
